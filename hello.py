af44cd1a0138e827eb43c0dbfbf39c6c6634a211
zzfQQ917 <heesun7120@gmail.com>
2025-01-18 20:01:19 +0900

    fif3jtiff

--- hello.py
+++ hello.py
@@ -1,2 +1,9 @@
-print('hello. ')
-s = int(input())
+def number(a,b,c):
+    multiply = a*b*c
+    print(multiply)
+    addition = a+b+c
+    print(addition)
+    classify = [a,b,c]
+    classify.sort(reverse=True)
+    return classify[2],classify[1],classify[0]
+print(number(1,2,3))
